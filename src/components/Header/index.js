90833c723e9426a7725c56baf53bf71f25e6427d
diatsilva007 <diatsilva007@gmail.com>
2025-05-02 16:03:41 -0300

    refatora o componente Header para incluir a imagem do logo e melhorar a estrutura do JSX

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,18 @@
-import './header.css';
-import { Link } from 'react-router-dom'
+import "./header.css";
+import { Link } from "react-router-dom";
+import logo from "../../img/logo-NerdFlix.png"; // Importando a imagem do logo
 
-function Header(){
-  return(
+function Header() {
+  return (
     <header>
-      <Link className="logo" to="/">Prime Flix</Link>
-      <Link className="favoritos" to="/favoritos">Meus filmes</Link>
+      <Link className="logo" to="/">
+        <img src={logo} alt="Logo" className="logo-img"/>
+      </Link>
+      <Link className="favoritos" to="/favoritos">
+        Meus filmes
+      </Link>
     </header>
-  )
+  );
 }
 
 export default Header;
